80135224f065f0e78fabfd230de043c9930afaf7
Plant <mythicalplanetdude@gmail.com>
2025-08-02 17:15:56 +1000

    update dividers

--- src/main/java/net/oceanias/opal/utility/extension/OStringExtension.java
+++ src/main/java/net/oceanias/opal/utility/extension/OStringExtension.java
@@ -8,13 +8,13 @@ import org.jetbrains.annotations.NotNull;
 @SuppressWarnings("unused")
 public final class OStringExtension {
     public static final String CHAT_DIVIDER_SHORT =
-        "<gray><strikethrough>                                                            </strikethrough>";
+        "&7&m                                                            ";
 
     public static final String CHAT_DIVIDER_LONG =
-        "<gray><strikethrough>                                                                              </strikethrough>";
+        "&7&m                                                                              ";
 
     public static final String LORE_DIVIDER =
-        "<dark_gray><strikethrough>                                              </strikethrough>";
+        "&8&m                                              ";
 
     public static @NotNull Component deserialize(final String string) {
         return deserializeToFormatted(string).component();
