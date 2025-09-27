54071bebfa1363d42af5e4b21dad2ef06dbc7171
Plant <mythicalplanetdude@gmail.com>
2025-09-27 11:31:16 +1000

    add &g

--- src/main/java/net/oceanias/opal/utility/extension/OStringExtension.java
+++ src/main/java/net/oceanias/opal/utility/extension/OStringExtension.java
@@ -25,19 +25,20 @@ public final class OStringExtension {
         Map.entry("&3", "&#00D1D1"), Map.entry("[dark_aqua]", "&#00D1D1"),
         Map.entry("&4", "&#D10000"), Map.entry("[dark_red]", "&#D10000"),
         Map.entry("&5", "&#880ED4"), Map.entry("[dark_purple]", "&#880ED4"),
-        Map.entry("&6", "&#FFA500"), Map.entry("[orange]", "&#FFA500"), Map.entry("[gold]", "&#FFA500"),
+        Map.entry("&6", "&#FFA500"), Map.entry("[orange]", "&#FFA500"),
         Map.entry("&9", "&#4444FF"), Map.entry("[blue]", "&#4444FF"),
         Map.entry("&a", "&#00FF00"), Map.entry("[green]", "&#00FF00"),
         Map.entry("&b", "&#00FFFF"), Map.entry("[aqua]", "&#00FFFF"),
         Map.entry("&c", "&#FF0000"), Map.entry("[red]", "&#FF0000"),
         Map.entry("&d", "&#FF00FF"), Map.entry("[magenta]", "&#FF00FF"), Map.entry("[light_purple]", "&#FF00FF"),
-        Map.entry("&e", "&#FFFF00"), Map.entry("[yellow]", "&#FFFF00")
+        Map.entry("&e", "&#FFFF00"), Map.entry("[yellow]", "&#FFFF00"),
+        Map.entry("&g", "&#FFD700"), Map.entry("[gold]", "&#FFD700")
     );
 
     private static final Pattern REPLACEMENTS_PATTERN = Pattern.compile(REPLACEMENTS_MAP.keySet().stream()
-        .map(Pattern::quote) // Escape special characters
-        .sorted((a,b) -> b.length() - a.length()) // Prioritise longer aliases
-        .reduce((a,b) -> a + "|" + b)
+        .map(Pattern::quote) // Escape special characters.
+        .sorted((a, b) -> b.length() - a.length()) // Prioritise longer aliases.
+        .reduce((a, b) -> a + "|" + b)
         .orElse("")
     );
 
