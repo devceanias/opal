fe04621a244c84c7450c72b331d6d704966d6ab2
Plant <87920028+preplant@users.noreply.github.com>
2025-11-26 12:02:19 +1100

    deprecate OCommandSenderExtension

--- src/main/java/net/oceanias/opal/utility/extension/OCommandSenderExtension.java
+++ src/main/java/net/oceanias/opal/utility/extension/OCommandSenderExtension.java
@@ -14,12 +14,15 @@ import lombok.experimental.ExtensionMethod;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
 
-// TODO: Add title builder and deprecate remaining methods.
+/**
+ * @deprecated Use builders instead.
+ * This class will be removed in a future version.
+ */
+@Deprecated(forRemoval = true)
 @SuppressWarnings("unused")
 @ExtensionMethod({ OStringExtension.class })
 @UtilityClass
 public final class OCommandSenderExtension {
-    @Deprecated
     public void messageDSR(
         @NotNull final CommandSender sender,
         final String message,
@@ -38,34 +41,28 @@ public final class OCommandSenderExtension {
         );
     }
 
-    @Deprecated
     public void messageDSR(
         final CommandSender sender, final String message, final boolean prefixed, final boolean lines
     ) {
         messageDSR(sender, message, prefixed, lines, false);
     }
 
-    @Deprecated
     public void messageDSR(final CommandSender sender, final String message, final boolean prefixed) {
         messageDSR(sender, message, prefixed, false, false);
     }
 
-    @Deprecated
     public void messageDSR(final CommandSender sender, final String message) {
         messageDSR(sender, message, true, false, false);
     }
 
-    @Deprecated
     public void messageDSR(final CommandSender sender) {
         messageDSR(sender, "", true, false, false);
     }
 
-    @Deprecated
     public void actionDSR(@NotNull final CommandSender sender, @NotNull final String text) {
         sender.sendActionBar(text.deserialize());
     }
 
-    @Deprecated
     public void soundDSR(
         @NotNull final CommandSender sender,
         @NotNull final org.bukkit.Sound sound,
@@ -79,7 +76,6 @@ public final class OCommandSenderExtension {
         );
     }
 
-    @Deprecated
     public void soundDSR(
         final CommandSender sender,
         final org.bukkit.Sound sound,
@@ -90,19 +86,16 @@ public final class OCommandSenderExtension {
         soundDSR(sender, sound, volume, pitch, emitter, Sound.Source.MASTER);
     }
 
-    @Deprecated
     public void soundDSR(
         final CommandSender sender, final org.bukkit.Sound sound, final float volume, final float pitch
     ) {
         soundDSR(sender, sound, volume, pitch, Sound.Emitter.self(), Sound.Source.MASTER);
     }
 
-    @Deprecated
     public void soundDSR(final CommandSender sender, final org.bukkit.Sound sound, final float volume) {
         soundDSR(sender, sound, volume, 1f, Sound.Emitter.self(), Sound.Source.MASTER);
     }
 
-    @Deprecated
     public void soundDSR(final CommandSender sender, final org.bukkit.Sound sound) {
         soundDSR(sender, sound, 1f, 1f, Sound.Emitter.self(), Sound.Source.MASTER);
     }
